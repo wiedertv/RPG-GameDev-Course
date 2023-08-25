22bb809435f9236afb361eb644ea044cb869620b
Alirio Angel <me@wiedertv.com>
2023-08-25 12:45:17 -0400

    Click to move the player

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -9,27 +9,29 @@ public class Mover : MonoBehaviour
     [Header("Target Info")]
     [field: SerializeField] private Transform target;
 
-
-    private Ray lastRay;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-         
-    }
-
-    // Update is called once per frame
     void Update()
     {
 
         if(Input.GetMouseButtonDown(0))
         {
-            // get main camera and use ScreenPointToRay in the mouse position, to know where the player is clicking.
-            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);         
+            MoveToCursor();
         }
 
-        Debug.DrawRay(lastRay.origin, lastRay.direction* 100);
         // GetComponent<NavMeshAgent>().destination = target.position;
+
+    }
+
+    private void MoveToCursor()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool isHit = Physics.Raycast(ray, out hit);
+
+        if (isHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
 
     }
 }
