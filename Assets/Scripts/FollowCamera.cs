daea45c0b0560f06124bd125b5e31543dd0e0d35
Alirio Angel <me@wiedertv.com>
2023-08-25 13:03:12 -0400

    Follow Camera (basic)

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    [field:Header("Target to follow")]
+    [SerializeField]
+    private Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position;   
+    }
+}
